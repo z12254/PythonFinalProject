fd53d7e5f5076654e25486c9011b150539c5073a
zjvog <zackvogl@buffalo.edu>
2025-08-11 12:38:48 -0400

    commit

--- Projects/Final Project.py
+++ Projects/Final Project.py
@@ -9,17 +9,15 @@ import requests
 
 
 API_KEY = "1e2818762094064f311ceb8819ae47fb"
-SYMBOL = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "BRK.B", "TSLA", "LLY", "AVGO",
-    "JPM", "V", "UNH", "JNJ", "WMT", "PG", "MA", "XOM", "HD", "MRK",
-    "ABBV", "ORCL", "COST", "CVX", "PEP", "BAC", "KO", "ADBE", "NVO", "TMO",
-    "NFLX", "ACN", "LIN", "MCD", "ABT", "DHR", "INTC", "TM", "VZ", "CSCO",
-    "DIS", "PFE", "NVS", "CRM", "T", "BHP", "SAP", "AMD", "RY", "SHEL"]
+SYMBOLS = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META","TSLA", "AMD", "INTC", "F"]
 DATE_FROM = '2025-06-28'
 DATE_TO = '2025-07-28'
+symbols_str = ",".join(SYMBOLS)
+
 url = (
     f"http://api.marketstack.com/v1/eod"
     f"?access_key={API_KEY}"
-    f"&symbols={symbol}"
+    f"&symbols={symbols_str}"
     f"&date_from={DATE_FROM}"
     f"&date_to={DATE_TO}")
 # these lines define the variables needed to pull the desired data from the API
